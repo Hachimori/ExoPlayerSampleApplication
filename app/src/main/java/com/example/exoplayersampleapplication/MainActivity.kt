e60779b726d88f9834d86002ec50fa65bd0e7dbd
Ben Hachimori <b.hachimori@goreha.com>
2020-11-02 09:30:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/exoplayersampleapplication/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.exoplayersampleapplication
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.upstream.AssetDataSource
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DataSpec
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*
+        val player = SimpleExoPlayer.Builder(this).build()
+        val cacheFile = File(cacheDir.canonicalPath + "/video_cache")
+        val cache = SimpleCache(
+            cacheFile,
+            LeastRecentlyUsedCacheEvictor(1024 * 1024 * 24),
+            ExoDatabaseProvider(this@MainActivity)
+        )
+        val cacheDataSource = CacheDataSource.Factory().apply {
+            setCache(cache)
+            setUpstreamDataSourceFactory(DefaultHttpDataSourceFactory(packageName))
+        }
+
+        playerView.player = player
+
+
+        player.setMediaSource(
+            ProgressiveMediaSource.Factory(cacheDataSource).createMediaSource(
+                MediaItem.fromUri(
+                    // "file:///android_asset/0040_ng.mp4"
+                    "https://tech-challenge-mobile.s3.eu-central-1.amazonaws.com/0040/0040.mp4"
+                )
+            )
+        )
+
+        player.prepare()
+*/
+
+        val player = SimpleExoPlayer.Builder(this).build()
+        playerView.player = player
+
+        val dataSpec = DataSpec(Uri.parse("asset:///android_asset/ABC2.mp4"))
+        val assetDataSource = AssetDataSource(this)
+        try {
+            assetDataSource.open(dataSpec)
+        } catch (e: AssetDataSource.AssetDataSourceException) {
+            e.printStackTrace()
+        }
+
+        val factory: DataSource.Factory = DataSource.Factory { assetDataSource }
+        val source: MediaSource = ExtractorMediaSource(
+            assetDataSource.uri!!,
+            factory, DefaultExtractorsFactory(), null, null
+        )
+        player.prepare(source)
+
+        //player.playWhenReady = true
+    }
+}
